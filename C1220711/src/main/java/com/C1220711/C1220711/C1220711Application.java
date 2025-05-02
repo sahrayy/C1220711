c3ac62381f7998a62d337f2e9dd9827070c2ba9f
sahra wadad yare <saam54457@gmail.com>
2025-05-02 15:35:08 +0100

    Add files via upload

---
+++ C1220711/src/main/java/com/C1220711/C1220711/C1220711Application.java
@@ -0,0 +1,13 @@
+package com.C1220711.C1220711;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1220711Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1220711Application.class, args);
+	}
+
+}
---
+++ C1220711/src/test/java/com/C1220711/C1220711/C1220711ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C1220711.C1220711;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1220711ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
